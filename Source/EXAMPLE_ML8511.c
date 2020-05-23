8cf447a5939a392b89a80cb2e7cc4667a6782b84
mjx4mois <mjx4mois@gmail.com>
2020-05-23 17:44:20 +0800

    update source code / example code

---
+++ Source/EXAMPLE_ML8511.c
@@ -0,0 +1,87 @@
+/*-----------------------------------------------------------------------
+     Creator		: Morris chiou
+     Sensor		: ML8511 UV light sensor
+     File Name		: EXAMPLE_ML8511.c
+     Function		: EXAMPLE_ML8511
+     Create Date	: 2017/12/31
+---------------------------------------------------------------------- */
+
+#ifndef __ML8511_EXAMPLE__
+#define __ML8511_EXAMPLE__  
+
+#include <mega32a.h>
+#include <stdio.h>
+#include <delay.h>
+#include <math.h>
+#include "alcd.h"
+#include "SENSOR_ML8511.h"
+
+void EXAMPLE_ML8511(void);
+
+
+/* This input use ADC channel */
+void EXAMPLE_ML8511(void)
+{
+          
+	FLOAT ml8511_raw_data=0.0, uv_data=0.0;
+	FLOAT get_data=uv_data;	
+	INT32U data[3]={0};
+	CHAR8U lcd_char_data[5]={0};
+
+	
+		printf("--------------------UV light sensor ML8511 --------------------\r\n");
+
+		/* initial EN pin IO */
+		ML8511_INIT();
+
+		while(1)
+		{
+				
+				/* Read ADC value */
+				ML8511_GET_RAW_DATA(&ml8511_raw_data);
+
+				/* Calculate The UV value */
+				ML8511_GET_UV_VALUE(ml8511_raw_data,&uv_data);
+
+				/* print the UV data */
+				data[0]= (INT32U)(uv_data);
+			  	data[1]= (INT32U)(uv_data*10) %10;   
+			  	data[2]= (INT32U)(uv_data*100) %10;   	  				
+				printf("ML8511 UV Level = %d.%d%d UV mW/cm2\r\n",data[0],data[1],data[2]);	/*unit: mW/cm2 */
+
+
+	                    lcd_char_data[0] = (CHAR8U)(uv_data/100)%10;     
+	                    lcd_char_data[1] = (CHAR8U)(uv_data/10)%10;
+	                    lcd_char_data[2] = (CHAR8U)(uv_data)%10;  
+	                    lcd_char_data[3] = (CHAR8U)(uv_data*10)%10;   
+	                    lcd_char_data[4] = (CHAR8U)(uv_data*100)%10;   	  
+
+
+			      /* show data */
+	                    lcd_gotoxy(0,0);
+			      lcd_putsf("UV:");
+				  
+	                    lcd_putchar(48+lcd_char_data[0]);
+	                    lcd_putchar(48+lcd_char_data[1]);
+	                    lcd_putchar(48+lcd_char_data[2]); 
+	                    lcd_putchar(46);    /* print "."  */ 
+	                    lcd_putchar(48+lcd_char_data[3]);  
+	                    lcd_putchar(48+lcd_char_data[4]); 
+
+			      /* show unit */
+			      lcd_gotoxy(5,1);
+			      lcd_putsf("mW/cm2");	/*unit: mW/cm2 */
+
+			  	/*Show IC ID */
+	    		      lcd_gotoxy(0,2);
+			      lcd_putsf("ML8511");	
+
+				  
+				delay_ms(300); /* tiny delay */
+		}
+
+
+               printf("-------------------- UV light sensor ML8511 --------------------\r\n");
+}
+
+#endif //#ifndef __ML8511_EXAMPLE__ 
---
+++ Source/SENSOR_ML8511.c
@@ -0,0 +1,106 @@
+/*-----------------------------------------------------------------------
+     Creator		: Morris chiou
+     Sensor		: ML8511 UV light sensor
+     File Name		: SENSOR_ML8511.c
+     Function		: SENSOR_ML8511
+     Create Date	: 2017/12/31
+---------------------------------------------------------------------- */
+
+#ifndef __ML8511_FUNCTION__
+#define __ML8511_FUNCTION__  
+
+#include <stdio.h>
+#include <math.h>
+#include <delay.h>
+
+#include "SENSOR_ML8511.h"
+
+#define ML8511_DEBUG		1
+
+/*** Note : ML8511 operation voltage -> 3.3V!*/
+
+/*** read adc_data[0] -> ADC channel 0 -> PA_ 2 pin*/
+extern unsigned int	adc_data[1];
+
+//********************************************* SYSTEM *************************************************
+/*--------------------------------------------------------------------------------------------------*/
+/* initial the IO pin to control ML8511 EN pin */
+/* ML8511 EN pin : active high */
+void ML8511_INIT(void)
+{
+	printf("ML8511_INIT initial \r\n");
+	DDRB = (1<<0);  		/* SET PORTB.0*/
+	PORTB.0=0;			/* SET low */
+	
+}
+/*--------------------------------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------------------------------*/
+/* read adc value & return ADC value ; note : adc -> 3V*/
+void ML8511_GET_RAW_DATA(FLOAT *raw_data)
+{
+		
+	#if ML8511_DEBUG	/* FOR DEBUG */
+		INT32U data[3]={0};  
+	#endif			
+		FLOAT get_data=0.0,temp_data=0.0;	
+		INT32U adc_raw_data=0;
+		/* control ML8511 EN pin high[enable]  */
+		PORTB.0=1;
+		/* wait ML8511 stable */
+		delay_ms(5);
+
+		/*get adc sample */
+		adc_raw_data=adc_data[0];
+		temp_data=(FLOAT)adc_raw_data;
+	
+		/* control ML8511 EN pin low[disable] */
+		PORTB.0=0;		
+		delay_ms(5);		
+		
+		get_data =  temp_data/1023 *3.3 ;	/*3.3V*/		
+		
+	#if ML8511_DEBUG	/* FOR DEBUG */
+		data[0]= (INT32U)(get_data);
+	  	data[1]= (INT32U)(get_data*10) %10;   
+	  	data[2]= (INT32U)(get_data*100) %10;   	  
+		printf("ADC RAW DATA = %d \r\n",adc_raw_data);	 
+		printf("ML8511 RAW DATA = %d.%d%d V\r\n",data[0],data[1],data[2]);	
+	#endif	
+		*raw_data =  get_data ;
+		
+}
+/* --------------------------------------------------------------------------------------------------*/
+/* --------------------------------------------------------------------------------------------------*/
+/*  calculate the raw data turn to UV Intensity */
+void ML8511_GET_UV_VALUE(FLOAT raw_data,FLOAT *uv_data)
+{ 
+
+	#if ML8511_DEBUG	/* FOR DEBUG */
+		INT32U data[3]={0};  
+	#endif		
+	
+	FLOAT get_data=0.0,temp_data=0.0;
+
+	if(raw_data<1) /* raw_data <1v */
+	{
+		get_data = 0;
+	}
+	else /* raw_data >1v */
+	{
+		get_data = 9.09*raw_data - 9.09 ;/* formula : x = 9.09y - 9.09 ; x-> UV  , y-> voltage */
+	}
+	
+	*uv_data =  get_data ;
+
+	#if ML8511_DEBUG	/* FOR DEBUG */
+		data[0]= (INT32U)(get_data);
+	  	data[1]= (INT32U)(get_data*10) %10;   
+	  	data[2]= (INT32U)(get_data*100) %10;   	  
+		 
+		printf("ML8511 uv_data = %d.%d%d V\r\n",data[0],data[1],data[2]);	
+	#endif	
+	
+}
+//--------------------------------------------------------------------------------------------------*/
+//********************************************* SYSTEM *************************************************
+#endif //#ifndef __ML8511_FUNCTION__ 
